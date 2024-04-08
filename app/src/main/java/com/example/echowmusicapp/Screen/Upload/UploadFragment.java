6829aa9a87b626abb1ed506134d56bf63369d2bf
Esdras Clother <Esdras.Clother@outlook.com>
2024-04-07 20:36:18 -0600

    avances en el codigo del UploadFragment para la subida de musica a firebase

--- app/src/main/java/com/example/echowmusicapp/Screen/Upload/UploadFragment.java
+++ app/src/main/java/com/example/echowmusicapp/Screen/Upload/UploadFragment.java
@@ -1,21 +1,64 @@
 package com.example.echowmusicapp.Screen.Upload;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.OpenableColumns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.echowmusicapp.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link UploadFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class UploadFragment extends Fragment {
+public class UploadFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    TextView textViewImage;
+    ProgressBar progressBar;
+    Spinner spinner;
+    Uri audioUri;
+    StorageReference mStorageref;
+    StorageTask mUploadTask;
+    DatabaseReference referenceSongs;
+    String songsCategory;
+    MediaMetadataRetriever metadataRetriever;
+    byte[] art;
+    String title1, artista1, album_art1 = "", duracion1;
+    TextView title, artista, album, dataa, duracion;
+    ImageView album_art;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,12 +98,153 @@ public class UploadFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_upload, container, false);
+
+        textViewImage = rootView.findViewById(R.id.textViewSongsFilesSelected);
+        progressBar = rootView.findViewById(R.id.progressbar);
+        title = rootView.findViewById(R.id.title);
+        artista = rootView.findViewById(R.id.artista);
+        duracion = rootView.findViewById(R.id.duracion);
+        album = rootView.findViewById(R.id.album);
+        dataa = rootView.findViewById(R.id.data);
+        album_art = rootView.findViewById(R.id.imageView);
+        spinner = rootView.findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        metadataRetriever = new MediaMetadataRetriever();
+        referenceSongs = FirebaseDatabase.getInstance().getReference().child("songs");
+        mStorageref = FirebaseStorage.getInstance().getReference().child("songs");
+
+        List <String> categorias = new ArrayList<>();
+        categorias.add("Música Romántica");
+        categorias.add("Pop de los 80");
+        categorias.add("Rock Alternativo");
+        categorias.add("Electrónica Chillout");
+        categorias.add("Hip-Hop Old School");
+        categorias.add("Musica de Cumpleaños");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, categorias);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+        return rootView;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
+
+        songsCategory = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(getActivity(), "Seleccionado: "+songsCategory, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void openAudioFiles(View view) {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.setType("*audio/*");
+        startActivityForResult(i, 101);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            audioUri = data.getData();
+            String fileNames = getFileName(audioUri);
+            textViewImage.setText(fileNames);
+            metadataRetriever.setDataSource(getActivity(), audioUri);
+
+            art = metadataRetriever.getEmbeddedPicture();
+            Bitmap bitmap = BitmapFactory.decodeByteArray(art,0,art.length);
+            album_art.setImageBitmap(bitmap);
+            album.setText(metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM));
+            artista.setText(metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST));
+            dataa.setText(metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_GENRE));
+            duracion.setText(metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            title.setText(metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE));
+
+            artista1 = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+            title1 = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+            duracion1 = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        }
+    }
+
+    private String getFileName(Uri uri){
+        String result = null;
+        if (uri.getScheme().equals("content")){
+            Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+
+            try {
+                if (cursor != null && cursor.moveToFirst()){
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            }
+            finally {
+                cursor.close();
+            }
+        }
+
+        if (result == null){
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut +1);
+            }
+        }
+        return result;
+    }
+
+    public void  uploadFileTofirebase(View view) {
+        if (textViewImage.equals("No hay archivo seleccionado.")){
+            Toast.makeText(getActivity(), "Por favor seleccione una imagen", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            if (mUploadTask != null && mUploadTask.isInProgress()){
+                Toast.makeText(getActivity(), "Carga de canciones en progreso", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                uploadFiles();
+            }
+        }
+    }
+
+    private void uploadFiles() {
+        if (audioUri != null) {
+            Toast.makeText(getActivity(), "Cargando, porfavor espera!", Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.VISIBLE);
+            final StorageReference storageReference = mStorageref.child(System.currentTimeMillis()+"."+getfileextension(audioUri));
+            mUploadTask = storageReference.putFile(audioUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    private String getfileextension(Uri audioUri){
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(audioUri));
     }
 }
