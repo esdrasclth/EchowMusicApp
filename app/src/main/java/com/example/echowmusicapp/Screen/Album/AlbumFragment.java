c03e76f9b9ae477ea9aa3f53c5162858163dacee
Esdras Clother <Esdras.Clother@outlook.com>
2024-04-08 23:44:06 -0600

    Completando funcionalidad de crear albunes y subir portadas a firebase

--- app/src/main/java/com/example/echowmusicapp/Screen/Album/AlbumFragment.java
+++ app/src/main/java/com/example/echowmusicapp/Screen/Album/AlbumFragment.java
@@ -1,21 +1,66 @@
 package com.example.echowmusicapp.Screen.Album;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.echowmusicapp.Models.Constants;
+import com.example.echowmusicapp.Models.Upload;
 import com.example.echowmusicapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AlbumFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AlbumFragment extends Fragment {
+public class AlbumFragment extends Fragment implements View.OnClickListener {
+
+    private Button buttomChoose, buttomUpload;
+    private EditText editTextName;
+    private ImageView imageView;
+
+    String songsCategory;
+    private static final int PICK_IMAGE_REQUEST = 234;
+
+    private Uri filePath;
+    StorageReference storageReference;
+    DatabaseReference mDatabase;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +106,138 @@ public class AlbumFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_album, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_album, container, false);
+
+        buttomChoose = rootView.findViewById(R.id.buttomChoose);
+        buttomUpload = rootView.findViewById(R.id.buttomupload);
+        editTextName = rootView.findViewById(R.id.edit_text);
+        imageView = rootView.findViewById(R.id.imageView);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference(Constants.DATABASE_PATH_UPLOADS);
+        Spinner spinner = rootView.findViewById(R.id.spinner);
+
+        List<String> categorias = new ArrayList<>();
+        categorias.add("Música Romántica");
+        categorias.add("Pop de los 80");
+        categorias.add("Rock Alternativo");
+        categorias.add("Electrónica Chillout");
+        categorias.add("Hip-Hop Old School");
+        categorias.add("Musica de Cumpleaños");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, categorias);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+        buttomUpload.setOnClickListener(this);
+        buttomChoose.setOnClickListener(this);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
+                songsCategory = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getActivity(), "Seleccionado: " +songsCategory, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        return rootView;
+    }
+
+    private ActivityResultLauncher<Intent> pickImageLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                    // Manejar el resultado aquí
+                    Uri imageUri = result.getData().getData();
+                    // Hacer lo que necesites con la URI de la imagen
+                }
+            }
+    );
+
+    @Override
+    public void onClick(View v) {
+        if (v == buttomChoose) {
+            showFileChoose();
+        } else if (v == buttomUpload) {
+            uploadFile(filePath);
+        }
+    }
+
+    private void uploadFile(Uri filePath) {
+        if (this.filePath != null) {
+            ProgressDialog progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setTitle("Cargando...");
+            progressDialog.show();
+            final StorageReference sRef = storageReference.child(Constants.STORAGE_PATH_UPLOADS + System.currentTimeMillis() + "." + getFileExtension(this.filePath));
+
+            sRef.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    sRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String url = uri.toString();
+                            Upload upload = new Upload(editTextName.getText().toString().trim(), url, songsCategory);
+                            String uploadId = mDatabase.push().getKey();
+                            mDatabase.child(uploadId).setValue(upload);
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity(), "Archivo subido correctamente!", Toast.LENGTH_SHORT).show();
+
+                            editTextName.setText("");
+                            imageView.setImageResource(android.R.color.transparent);
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                    double progress = (100.00 * snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                    progressDialog.setMessage("Cargando " + ((int)progress)+"%...");
+                }
+            });
+        }
+    }
+
+    private void showFileChoose() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent, "Seleccionar imagen"), PICK_IMAGE_REQUEST);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+                // Aquí llamas al método uploadFile() pasando el filePath
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getFileExtension(Uri uri) {
+        ContentResolver cr = getActivity().getContentResolver();
+        MimeTypeMap mime  = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(uri));
     }
 }
